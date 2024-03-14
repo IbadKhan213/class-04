7337776b850cbebe1b8a881255ff417aa02ce2dc
IbadKhan213 <160883583+IbadKhan213@users.noreply.github.com>
2024-03-15 00:15:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//Comment.There are two types 
+//1.Single line comment
+/*
+2.
+Multi line comment
+*/
+//ERROR
+/*
+1.Syntax Error
+2.Type Error
+3.Assignability Error
+*/
+//OPERATORS
+var num1 = 10;
+var num2 = 4;
+//Addition(+):
+var add = num1 + num2;
+//Subtraction(-):
+var sub = num1 - num2;
+//Multiplication(*):
+var mult = num1 * num2;
+//Division(/);
+var div = num1 / num2;
+//Modulus(%): means reminder:
+var mod = num1 % num2;
+//Exponentation(**): means power:
+var exp = Math.pow(num1, num2);
+//  Operation Perform
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
+console.log(exp);
---
+++ main.ts
@@ -0,0 +1,52 @@
+//Comment.There are two types 
+
+//1.Single line comment
+/*
+2.
+Multi line comment
+*/
+
+
+
+//ERROR
+
+/*
+1.Syntax Error
+2.Type Error
+3.Assignability Error
+*/
+
+
+
+//OPERATORS
+
+let num1 = 10;
+let num2 = 4;
+
+//Addition(+):
+let add = num1 + num2;
+
+//Subtraction(-):
+let sub = num1 - num2;
+
+//Multiplication(*):
+let mult = num1 * num2;
+
+//Division(/);
+let div = num1 / num2;
+
+//Modulus(%): means reminder:
+let mod = num1 % num2;
+
+//Exponentation(**): means power:
+let exp = num1 ** num2;
+
+//  Operation Perform
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
+console.log(exp);
+
+
